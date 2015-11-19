bf5db68d3a13fd7175aa4f1405b39c2c994514ac
Marcus Gartner <marcus@lob.com>
2015-11-19 00:48:31 -0800

    updated package information

--- mix.exs
+++ mix.exs
@@ -8,11 +8,16 @@ defmodule Excaliper.Mixfile do
       source_url: "https://github.com/mgartner/excaliper",
       version: "0.0.1",
       elixir: "~> 1.1",
-      description: "Fast image dimension parser inspired by the Node.JS module Calipers.",
+      description: "Fast image dimension detector inspired by the Node.JS module Calipers.",
+      package: [
+        maintainers: ["Marcus Gartner"],
+        licenses: ["MIT"],
+        links: %{ "GitHub" => "https://github.com/mgartner/excaliper" },
+      ],
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
+      test_coverage: [tool: ExCoveralls],
       deps: deps,
-      test_coverage: [tool: ExCoveralls]
     ]
   end
 
